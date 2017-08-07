4ab72b4fde0d57efe20f8d652162bbe7ffb0b9df
godza-kauyani <kauyani-godfreygmail.com>
2017-08-07 13:48:31 +0200

    corrected express app

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const app = express()
-const port = process.env.PORT || 8080;
 
+const  port = process.env.PORT || 8080;
 app.use(express.static(__dirname));
 
-app.get('/',(res,req)=>{
+app.get('/',(req,res)=>{
 	res.send ("Miss you my love");
 });
-
-app.listen(port);
+console.log(port)
